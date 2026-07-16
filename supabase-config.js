a0d7b0ec57490399d5fc36aad78a211bd3e4e0cf
onlinemkttkn <onlinemkt.tkn@gmail.com>
2026-07-16 22:56:15 +0700

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,8 +1,17 @@
-// Supabase → Project Settings → API
-const SUPABASE_URL = "https://wkozeuxyhqcmiatssviq.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_NENuHV7DiTHuYcQ6Sle6Xg_mhUfd5SZ";
+const SUPABASE_URL =
+  "https://wkozeuxyhqcmiatssviq.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY =
+  "sb_publishable_NENuHV7DiTHuYcQ6Sle6Xg_mhUfd5SZ";
 
 const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
-  SUPABASE_PUBLISHABLE_KEY
+  SUPABASE_PUBLISHABLE_KEY,
+  {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+      detectSessionInUrl: true
+    }
+  }
 );
