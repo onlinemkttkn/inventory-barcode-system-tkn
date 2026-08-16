c2237ff71138b4a771cf651c0ca440354c2f077f
onlinemkttkn <onlinemkt.tkn@gmail.com>
2026-08-16 17:51:28 +0700

    Add files via upload

--- service-worker-v5.31.28.js
+++ service-worker-v5.31.28.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const VERSION = '5.31.28-HF10';
+const VERSION = '5.31.28-HF12';
 const STATIC_CACHE = `tkn-static-${VERSION}`;
 const PAGE_CACHE = `tkn-pages-${VERSION}`;
 const CORE_ASSETS = [
